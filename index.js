be93b1e180de8f92b70fc431abb70bf991f8e851
Silas Sefas <silassefas23@gmail.com>
2024-10-05 10:23:50 -0300

    Enviando o arquivo

---
+++ index.js
@@ -0,0 +1,21 @@
+let nomeHeroi = "Felipe JS"
+let xpHeroi = 10000
+
+if (xpHeroi <= 1.000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Ferro")
+}else if(xpHeroi >= 1.001 && xpHeroi <= 2000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Bronze")
+}else if(xpHeroi >= 2.001 && xpHeroi <= 5000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Prata")
+}else if(xpHeroi >= 5.001 && xpHeroi <= 7000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Ouro")
+}else if(xpHeroi >= 7.001 && xpHeroi <= 8000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Platina")
+}else if(xpHeroi >= 8.001 && xpHeroi <= 9000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Ascendente")
+}else if(xpHeroi >= 9.001 && xpHeroi <= 10000){
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Imortal")
+}else{
+    console.log("O Herói de nome "+ nomeHeroi +" está no nível de Radiante")
+}
+
